fa38bb1f18e083c68be9c1c1ba582f3a14994bee
Francisco Rivera <fran.rivera.saborido@gmail.com>
2023-07-27 21:53:28 +0200

    entrega4

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,32 @@
-import "./style.css";
+function sumar() {
+    const sumar1 = (document.getElementById("siguiente")as HTMLInputElement).value;
 
-console.log("Hello Typescript!");
+    const resultado = parseInt(sumar1) + 1;
+
+    const resultadoElement = document.getElementById("resultado");
+    if(resultadoElement !== null && resultadoElement !== undefined){
+    resultadoElement.innerHTML = resultado.toString();
+    }
+}
+
+function restar(){
+    const restar1 = (document.getElementById("anterior")as HTMLInputElement).value;
+
+    const resultado= parseInt(restar1) - 1;
+
+    const resultadoElement = document.getElementById("resultado");
+    if(resultadoElement !== null && resultadoElement !== undefined){
+    resultadoElement.innerHTML = resultado.toString();
+    }
+}
+
+const botonSiguiente = document.getElementById("siguiente");
+if (botonSiguiente!== null && botonSiguiente !== undefined){
+botonSiguiente.addEventListener("click", sumar);
+}
+
+const botonAnterior = document.getElementById ("anterior");
+if (botonAnterior !== null && botonAnterior !== undefined){
+botonAnterior.addEventListener("click", restar);
+}
+let resultado= 1

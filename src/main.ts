cf987cf02d6601df42473f64e3bcef38b52705e9
Francisco Rivera <fran.rivera.saborido@gmail.com>
2023-07-31 16:46:04 +0200

    actualizacion1

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,31 @@
+let resultado = 0
 function sumar() {
-    const sumar1 = (document.getElementById("siguiente")as HTMLInputElement).value;
+    // Leer los valores de los inputs
+    const sumando1 = (document.getElementById("siguiente") as HTMLButtonElement)
+    .value;
+     resultado = resultado +1;
 
-    const resultado = parseInt(sumar1) + 1;
-
-    const resultadoElement = document.getElementById("resultado");
-    if(resultadoElement !== null && resultadoElement !== undefined){
-    resultadoElement.innerHTML = resultado.toString();
+    const resultadoElement= document.getElementById("resultado");
+    if (resultadoElement !== null && resultadoElement !== undefined) {
+        resultadoElement.innerHTML=resultado.toString();
     }
 }
+function restar() {
+    
+    const restarUno = (document.getElementById("anterior") as HTMLButtonElement)
+    .value;
+    
+     resultado = resultado -1;
+    
 
-function restar(){
-    const restar1 = (document.getElementById("anterior")as HTMLInputElement).value;
-
-    const resultado= parseInt(restar1) - 1;
-
-    const resultadoElement = document.getElementById("resultado");
-    if(resultadoElement !== null && resultadoElement !== undefined){
-    resultadoElement.innerHTML = resultado.toString();
+    const resultadoElement= document.getElementById("resultado");
+    if (resultadoElement !== null && resultadoElement !== undefined) {
+        resultadoElement.innerHTML=resultado.toString();
     }
 }
+const botonSumar= document.getElementById("siguiente");
 
-const botonSiguiente = document.getElementById("siguiente");
-if (botonSiguiente!== null && botonSiguiente !== undefined){
-botonSiguiente.addEventListener("click", sumar);
+if (botonSumar != null && botonSumar !== undefined){
+    botonSumar.addEventListener("click", sumar)
 }
-
-const botonAnterior = document.getElementById ("anterior");
-if (botonAnterior !== null && botonAnterior !== undefined){
-botonAnterior.addEventListener("click", restar);
-}
-let resultado= 1
+const botonRestar= document.getElementById("anterior");
